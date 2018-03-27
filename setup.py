894ac1179a2aa86d2b33be935e7105de1e0afcbc
Aamish Baloch <aamish.baloch@gmail.com>
2018-03-27 19:19:30 +0500

    Version Bump

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='progress-edx-platform-extensions',
-    version='1.0.2',
+    version='1.0.6',
     description='Progress management extension for edX platform',
     long_description=open('README.rst').read(),
     author='edX',
