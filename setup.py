2d111f94c8f13b06d871a3b7d49455088ddccfbc
Nasir Hussain <nasirhjafri@gmail.com>
2020-01-14 12:02:55 +0500

    Updated version to 1.0.9

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='progress-edx-platform-extensions',
-    version='1.0.8',
+    version='1.0.9',
     description='Progress management extension for edX platform',
     long_description=open('README.rst').read(),
     author='edX',
